fecd6b420309168ebefe4fb42937acc97b2ae676
Jean-Paul van Ravensberg <14926452+Cloudenius@users.noreply.github.com>
2020-08-13 19:17:47 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     name="ziggostatus",
     packages=find_packages(include=["ziggostatus"]),
     url="https://github.com/DevSecNinja/ZiggoStatus",
-    version="0.1.0",
+    version="0.1.1",
     zip_safe=False,
 )
